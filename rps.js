d1ca33eda41db4ec2667d756c03ffd08a1bff1d8
neilt <neiltran.syd@gmail.com>
2025-05-26 17:15:05 +1000

    complete UI

--- rps.js
+++ rps.js
@@ -3,7 +3,8 @@ let humanScore = 0;
 let computerScore = 0;
 let humanSelection;
 let computerSelection;
-
+const round = document.querySelector(".round");
+//(1) function to get the computer choice from math random
 function getComputerChoice() {
     let r = Math.random();
     let computerSelection;
@@ -19,50 +20,34 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-
-
-let buttons = document.querySelectorAll("button");
- buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        humanSelection = this.textContent.toLowerCase();
-        playRound();
-    let currentScore = "Current score: " + humanScore + " - " + computerScore;
-    console.log(currentScore);
-    });
- });
-
-
-
-
-
-
-function playRound() {
+//(2) function to compare human choice and computer choice
+function playRound() {    
 let computerChoice = getComputerChoice();
 let humanChoice = humanSelection;
 if (humanChoice === 'rock') {
     if (computerChoice === 'rock') {
-        console.log('Tie! We both got Rock!');
+        round.textContent = 'Tie! We both got Rock!';
     }
     if (computerChoice === 'paper') {
-        console.log('You lost! Paper beats Rock!');
+        round.textContent = 'You lost! I got Paper, and Paper beats Rock!';
         computerScore += 1;
     }
     if (computerChoice === 'scissors') {
-        console.log('You won! Rock beats Scissors!');
+        round.textContent = 'You won! I got Scissors, and Rock beats Scissors!';
         humanScore += 1;
     }
 }
 
 if (humanChoice === 'paper') {
     if (computerChoice == 'paper') {
-        console.log('Tie! We both got Paper!');
+        round.textContent = 'Tie! We both got Paper!';
     }
     if (computerChoice === 'scissors') {
-        console.log('You lost! Scissors beats Paper!');
+        round.textContent = 'You lost! I got Scissors, and Scissors beats Paper!';
         computerScore += 1;
     }
     if (computerChoice === 'rock') {
-        console.log('You won! Paper beats Rock!');
+        round.textContent = 'You won! I got Rock, and Paper beats Rock!';
         humanScore += 1;
     }
     }
@@ -70,18 +55,68 @@ if (humanChoice === 'paper') {
 
 if (humanChoice === 'scissors') {
     if (computerChoice == 'scissors') {
-        console.log('Tie! We both got Scissors!');
+        round.textContent = 'Tie! We both got Scissors!';
     }
     if (computerChoice == 'rock') {
-        console.log('You lost! Rock beats Scissors!');
+        round.textContent = 'You lost! I got Rock, and Rock beats Scissors!';
         computerScore += 1;
     }
     if (computerChoice == 'paper') {
-        console.log('You won! Scissors beats Paper!');
+        round.textContent = 'You won! I got Paper, and Scissors beats Paper!';
         humanScore += 1;
     }
     }
 
 }
+
+//(3) call the functions, check anyone reachs 5 wins, and anounce final result
+let buttons = document.querySelectorAll("button");
+    buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        humanSelection = this.textContent.toLowerCase();
+        playRound();
+    const div = document.querySelector(".score");
+    var replay = document.querySelector(".replay");
+    if (humanScore < 5 && computerScore < 5) {
+    let currentScore = "Current score: " + humanScore + " - " + computerScore;
+    div.textContent = currentScore;
+    }
+    if (humanScore >=5) {
+        div.textContent = 'You WON! You are the Rock Paper Scissors Champion!'
+        humanScore = 0;
+        computerScore = 0;
+        let button = document.createElement('button');
+        replay.appendChild(button);
+        button.textContent = 'Replay';
+        button.addEventListener("click", function() {
+            replay.removeChild(button);
+            div.textContent = '';
+            round.textContent = '';
+        }
+        )
+    }
+    if (computerScore>=5) {
+        div.textContent = 'You LOST! Do you want to try again?'
+        humanScore = 0;
+        computerScore = 0;
+         let button = document.createElement('button');
+        replay.appendChild(button);
+        button.textContent = 'Replay';
+        button.addEventListener("click", function() {
+            replay.removeChild(button);
+            div.textContent = '';
+            round.textContent = '';
+        })
+    }
+    })
+    });
+
+
+
+
+
+
+
+
 
 
